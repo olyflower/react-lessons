e4a453144089ad3b2d5ada62f8701ad9c90278b7
olyflower <2013.olga.yudina@gmail.com>
2024-07-14 11:26:53 +0300

    add api

--- hw_5/pizza-app/src/components/MenuList/MenuList.jsx
+++ hw_5/pizza-app/src/components/MenuList/MenuList.jsx
@@ -1,17 +1,28 @@
-import React from "react";
-import pizzas from "../../services/data";
+import React, { useState, useEffect } from "react";
 import MenuItem from "../MenuItem/MenuItem";
-import Header from "../Header/Header"
+import Header from "../Header/Header";
+import service from "../../services/services";
 import style from "../../components/MenuList/MenuList.module.css";
 
 export default function MenuList() {
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		const getData = async () => {
+			const response = await service.get();
+			setData(response.data);
+		};
+
+		getData();
+	}, []);
+
 	return (
 		<>
 			<Header />
 			<div className={style.container}>
-				<div className={style.wrapper} >
+				<div className={style.wrapper}>
 					<ul>
-						{pizzas.map((item, index) => (
+						{data.map((item, index) => (
 							<MenuItem item={item} key={index} />
 						))}
 					</ul>

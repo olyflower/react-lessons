9fdd3e156ec0972935301545b81d35996d5f1fa0
olyflower <2013.olga.yudina@gmail.com>
2024-07-14 23:20:54 +0300

    replace logic to menu.jsx

--- hw_5/pizza-app/src/components/MenuList/MenuList.jsx
+++ hw_5/pizza-app/src/components/MenuList/MenuList.jsx
@@ -1,21 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import MenuItem from "../MenuItem/MenuItem";
 import Header from "../Header/Header";
-import service from "../../services/services";
 import style from "../../components/MenuList/MenuList.module.css";
 
-export default function MenuList() {
-	const [data, setData] = useState([]);
-
-	useEffect(() => {
-		const getData = async () => {
-			const response = await service.get();
-			setData(response.data);
-		};
-
-		getData();
-	}, []);
-
+export default function MenuList({ data }) {
 	return (
 		<>
 			<Header />
